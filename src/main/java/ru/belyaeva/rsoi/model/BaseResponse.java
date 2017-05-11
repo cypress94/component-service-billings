89426acd43657c27de0ac1e4f192074fcd1e4e4d
Ox <cypress94@mail.ru>
2017-05-11 23:10:06 +0300

    1

---
+++ src/main/java/ru/belyaeva/rsoi/model/BaseResponse.java
@@ -0,0 +1,13 @@
+package ru.belyaeva.rsoi.model;
+
+import lombok.Data;
+
+/**
+ * Created by user on 18.12.2016.
+ */
+@Data
+public class BaseResponse {
+    private Boolean errorCode;
+    private String errorMessage;
+
+}
---
+++ src/main/java/ru/belyaeva/rsoi/service/BillingService.java
@@ -0,0 +1,17 @@
+package ru.belyaeva.rsoi.service;
+
+import org.springframework.stereotype.Service;
+import ru.belyaeva.rsoi.model.*;
+
+/**
+ * Created by user on 22.11.2016.
+ */
+@Service
+public interface BillingService {
+
+    BillingResponse executeBilling(Double cost, Long userId);
+    BillingResponse getBilling(Long userId, Long billingId);
+    BaseResponse createUserBilling(UserBillingInfo userBillingInfo);
+    BaseResponse returnBilling(Long userId, Long billingId);
+    BaseResponse deleteBilling(Long billingId);
+}
